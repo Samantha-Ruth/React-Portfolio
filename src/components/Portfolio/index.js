f6b7d48859254312093c5e2d446da379ad233d60
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-25 19:01:40 -0600

    updated github links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,7 +4,6 @@ import recipe from "../../assets/projects/Recipe.png";
 import healthcare_Hero from "../../assets/projects/Healthcare-Hero.PNG";
 import COVID from "../../assets/projects/COVID_by_Country.jpg";
 import budget from "../../assets/projects/Budget_tracker.png";
-import code from "../../assets/projects/Code_quiz.jpg";
 
 function Portfolio() {
   return (
@@ -16,7 +15,7 @@ function Portfolio() {
             <img className="image" src={recipe}   style={{ width: "90%" }} alt="profile" />
             <Container fluid className="prj1">
               <Grid display="flex" justifyContent="center">Compose a meal with what's left in your fridge!</Grid>
-              <Link display="flex" justifyContent="center" href="https://github.com/Samantha-Ruth/recipe-gen" target="_blank" rel="noreferrer">GitHub</Link>
+              <Link display="flex" justifyContent="center" href="https://github.com/Samantha-Ruth/supper-snacks" target="_blank" rel="noreferrer">GitHub</Link>
             </Container>
         </Grid>
         <Grid xs={5} lg={5} className="project1">
@@ -24,7 +23,7 @@ function Portfolio() {
             <img src={COVID} className="px-1 mx-2" alt="profile" />
             <Container fluid className="prj2">
               <Grid display="flex" justifyContent="center" >Front-end application that provides COVID-19 data per country.</Grid>
-              <Link display="flex" justifyContent="center" href="https://github.com/Samantha-Ruth/recipe-gen" target="_blank" rel="noreferrer">GitHub</Link>
+              <Link display="flex" justifyContent="center" href="https://github.com/Samantha-Ruth/Group-Covid-Project" target="_blank" rel="noreferrer">GitHub</Link>
             </Container>
         </Grid>
         <Grid xs={5} lg={5} className="project1">
@@ -32,7 +31,7 @@ function Portfolio() {
             <img src={healthcare_Hero} className="px-1 mx-2" alt="profile" />
             <Container fluid className="prj1">
               <Grid display="flex" justifyContent="center">Web application to help patients compare the cost of a service across different medical providers.</Grid>
-              <Link display="flex" justifyContent="center" href="https://github.com/Samantha-Ruth/recipe-gen" target="_blank" rel="noreferrer">GitHub</Link>
+              <Link display="flex" justifyContent="center" href="https://github.com/Samantha-Ruth/Healthcare-Hero" target="_blank" rel="noreferrer">GitHub</Link>
             </Container>
         </Grid>
         <Grid xs={5} lg={5} className="project1">
@@ -44,7 +43,7 @@ function Portfolio() {
               />
               <Container fluid className="prj1">
               <Grid display="flex" justifyContent="center">Keep track of your finances!</Grid>
-              <Link display="flex" justifyContent="center" href="https://github.com/Samantha-Ruth/recipe-gen" target="_blank" rel="noreferrer">GitHub</Link>
+              <Link display="flex" justifyContent="center" href="https://github.com/Samantha-Ruth/Budget-Tracker" target="_blank" rel="noreferrer">GitHub</Link>
             </Container>
         </Grid>
       </Grid>
