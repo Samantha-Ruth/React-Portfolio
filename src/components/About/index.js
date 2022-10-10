a1002f74f616e8af3e58984a48d8a3a3153756c2
Samantha-Ruth <sam.haberman@gmail.com>
2022-10-09 22:43:41 -0600

    changing color scheme

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,28 +6,17 @@ import { Grid, Box } from "@mui/material";
 const About = () => {
   return (
     <div>
-      <section>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            height: "5vw",
-          }}
-          className="about"
-        >
-          <h1 id="about">About Me</h1>
-        </div>
-      </section>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container className="flex-row">
           <Grid container>
+            <Grid container>
             <img
               src={profileImage}
-              className="py-1 my-2"
-              style={{ width: "25%" }}
               alt="profile"
+              className="py-1 my-2"
+              style={{ width: "35%" }}
             />
-            <p className="px-2 my-5" style={{ width: "75%" }}>
+            <p className="about_text1 px-2 my-5" style={{ width: "65%" }}>
               Full stack developer, focused on taking difficult aspects of a job
               and making them more streamlined, organized, cleaner, and
               efficient. Born in the mountains, I have been working in the ski
@@ -38,13 +27,18 @@ const About = () => {
               developing programs to make this work easier, including a tracking
               system for explosives, an automated scheduler, and an
               easy-to-follow evacuation plan that pulls everyone together with a
-              common goal. While I’ve been a developer my whole life, I’ve
+              common goal.
+              </p>
+            </Grid>
+            <Grid className="px-2">
+            <p className="about_text px-2"> While I’ve been a developer my whole life, I’ve
               recently gotten more serious and attained a certificate from
               Columbia Engineering bootcamp. I enjoy challenges and solving
               problems, and coding provides a platform of endless learning as
               well as tangible accomplishment. I want to learn and grow as a
               developer, using best practices to solve real world problems.{" "}
             </p>
+            </Grid>
           </Grid>
           <Grid container justifyContent="center" style={{ width: "100%" }}>
             <div className="icons">
