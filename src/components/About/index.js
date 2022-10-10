10e2d3bb612d9f07e46fcce6da2fda302ecc0a11
Samantha-Ruth <sam.haberman@gmail.com>
2022-10-09 22:01:41 -0600

    center icons

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -28,7 +28,124 @@ const About = () => {
               alt="profile"
             />
             <p className="px-2 my-5" style={{ width: "75%" }}>
-            Full stack developer, focused on taking difficult aspects of a job and making them more streamlined, organized, cleaner, and efficient.  Born in the mountains, I have been working in the ski industry most of my career, setting off avalanches before the public accidentally does, training others in tactical high angle-rescue, and helping sick and injured patients on the slope.  I’ve devoted most of my life to heavy lifting, hard work, and developing programs to make this work easier, including a tracking system for explosives, an automated scheduler, and an easy-to-follow evacuation plan that pulls everyone together with a common goal. While I’ve been a developer my whole life, I’ve recently gotten more serious and attained a certificate from Columbia Engineering bootcamp.  I enjoy challenges and solving problems, and coding provides a platform of endless learning as well as tangible accomplishment.  I want to learn and grow as a developer, using  best practices to solve real world problems. </p>
+              Full stack developer, focused on taking difficult aspects of a job
+              and making them more streamlined, organized, cleaner, and
+              efficient. Born in the mountains, I have been working in the ski
+              industry most of my career, setting off avalanches before the
+              public accidentally does, training others in tactical high
+              angle-rescue, and helping sick and injured patients on the slope.
+              I’ve devoted most of my life to heavy lifting, hard work, and
+              developing programs to make this work easier, including a tracking
+              system for explosives, an automated scheduler, and an
+              easy-to-follow evacuation plan that pulls everyone together with a
+              common goal. While I’ve been a developer my whole life, I’ve
+              recently gotten more serious and attained a certificate from
+              Columbia Engineering bootcamp. I enjoy challenges and solving
+              problems, and coding provides a platform of endless learning as
+              well as tangible accomplishment. I want to learn and grow as a
+              developer, using best practices to solve real world problems.{" "}
+            </p>
+          </Grid>
+          <Grid container justifyContent="center" style={{ width: "100%" }}>
+            <div className="icons">
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
+                alt="github icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
+                alt="html5 icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"
+                alt="css3 icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                alt="javascript icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg"
+                alt="jquery icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg"
+                alt="bootstap icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+                alt="nodejs icon"
+                style={{ width: "6%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg"
+                alt="npm icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg"
+                alt="heroku"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+                alt="express icon"
+                style={{ width: "6%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
+                alt="mysql icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg"
+                alt="sequelize icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+                alt="mongodb icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                alt="react icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain-wordmark.svg"
+                alt="graphql icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
+                alt="redux icon"
+                style={{ width: "5%", padding: "0.5%" }}
+                className="img-icon"
+              />
+            </div>
           </Grid>
         </Grid>
       </Box>
